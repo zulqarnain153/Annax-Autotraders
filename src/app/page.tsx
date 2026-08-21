5b7d941ba38cf9044138917fb292d3dc31cbb165
zulqarnain153 <zulqarnaintahir153@gmail.com>
2026-08-22 01:10:39 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import { FeaturedCars } from "@/components/home/FeaturedCars";
 import { LatestArrivals } from "@/components/home/LatestArrivals";
 import { WhyChooseUs } from "@/components/home/WhyChooseUs";
 import { PartExchangeTeaser } from "@/components/home/PartExchangeTeaser";
-import { Testimonials } from "@/components/home/Testimonials";
 import { RecentlySold } from "@/components/home/RecentlySold";
-import { StatsSection } from "@/components/home/StatsSection";
 import { LatestNews } from "@/components/home/LatestNews";
 import { ContactSection } from "@/components/home/ContactSection";
 
@@ -17,9 +15,7 @@ export default function HomePage() {
       <LatestArrivals />
       <WhyChooseUs />
       <PartExchangeTeaser />
-      <Testimonials />
       <RecentlySold />
-      <StatsSection />
       <LatestNews />
       <ContactSection />
     </>
